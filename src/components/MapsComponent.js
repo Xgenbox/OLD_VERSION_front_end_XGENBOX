7f022ef2b75c130c0dc1f2a99c1bfb68ddb71348
Zied bousnina <zbousnina@yahoo.com>
2023-12-05 21:33:12 +0100

    I have removed a create request from the company dashborad

--- src/components/MapsComponent.js
+++ src/components/MapsComponent.js
@@ -175,19 +175,19 @@ function MapsComponent() {
                   // lg="6"
                     md="2"
                   >
-                     <Link
+                     {/* <Link
                           to={`/admin/AddRequest`}
                           >
                             <Button
                             className="float-right"
-                            // color="primary"
+
                             >
 
 
                 Create a Request
                 <i className=" ml-2 fas fa-arrow-right" />
                             </Button>
-                          </Link>
+                          </Link> */}
                   </Col>
                 </Row>
               </CardHeader>
